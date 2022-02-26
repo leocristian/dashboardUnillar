607fd81e03d9b539f0f5c025d903f74fe556c3e0
leocristian <leonardosclopes@gmail.com>
2022-02-26 12:29:12 -0300

    add vue core

---
+++ dashboard/src/pages/landing/DashboardCore.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <CRow :md="12">
+            <h1>oi</h1>
+        </CRow>
+    </div>
+</template>
--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHashHistory} from 'vue-router';
 
 import HomePage from '../pages/landing/HomePage.vue'
 import SigninPage from '../pages/signin/SigninPage.vue'
-import DemoBoard from '../pages/landing/DemoBoard.vue'
+// import DemoBoard from '../pages/landing/DemoBoard.vue'
+import DashboardCore from '../pages/landing/DashboardCore.vue'
 const routes = [
     { path: '/', component: HomePage },
     { path: '/signin', component: SigninPage },
-    { path: '/demo', component: DemoBoard}
+    { path: '/demo', component: DashboardCore.vue}
 ]
 
 const router = createRouter({
